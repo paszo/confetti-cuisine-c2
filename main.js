853dae84031483811e52e87a7b999be56bc8c1d4
paszo <paszo2008@gmail.com>
2020-07-19 09:10:48 +0200

    Internal working - not pretty

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+const layouts = require('express-ejs-layouts');
+
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Confetti Cuisine");
-});
+app.use(express.static("public"));
+app.use(express.urlencoded( {extended: false}));
+app.use(express.json());
+app.use(layouts);
+
+app.get("/", homeController.showHome);
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
 
 app.listen(app.get("port"), () => {
     console.log(`Server is running at port ${app.get("port")} ...`);
